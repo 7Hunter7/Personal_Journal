8feb2c1b9d41645e5f83dcb4c9130f4cc2dae16d
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-29 21:03:29 +0300

    UPD: git inited

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./App.css";
+
+function App() {
+  return <>
+    <h1>Заголовок</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque at sequi possimus inventore doloribus vel quos aut officiis repellat quod? Ratione esse optio ut commodi quasi explicabo placeat laudantium animi.</p>
+    <button></button>
+  </>; 
+}
+
+export default App;
