0156d8fd802bfaae09ead018be2a7c95ff296508
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-30 17:49:38 +0300

    UPD(JournalForm): using 'useState()' for form

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -5,17 +5,25 @@ import Button from '../Button/Button';
 function JournalForm() {
 	const [inputData, setInputData] = useState('');
 
-	const inputText = (event) => {
-		console.log(event.target.value);
-		setInputData(event.target.value)
+	const inputText = (e) => {
+		console.log(e.target.value);
+		setInputData(e.target.value)
+	}
+
+	const addJournalItem = (e) => {
+		e.preventDefault();
+		const formData = new FormData(e.target);
+		const formProps = Object.fromEntries(formData)
+		console.log(formProps);
 	}
 
 	return (
-		<form className='journal-form'>
-			<input type="date"/>
-			<input type="text" value={inputData} onChange={inputText}/>
-			<textarea name="" id="" cols={30} rows={10}></textarea>
-			<Button/>
+		<form className='journal-form' onSubmit={addJournalItem}>
+			<input type='text' name='title'/>
+			<input type='date' name='date'/>
+			<input type='text' name='tag' value={inputData} onChange={inputText}/>
+			<textarea name='post' id="" cols='30' rows='10'></textarea>
+			<Button text='Сохранить'/>
 		</form>
 
 	);
