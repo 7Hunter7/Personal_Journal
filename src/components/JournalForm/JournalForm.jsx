eb0c8204ce4300bf6ee6815093fe3e44012770e1
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-30 18:17:20 +0300

    UPD(JournalForm): updated addJournalItem() using onSubmit(formProps)

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,19 +1,12 @@
 import './JournalForm.scss';
-import {useState} from 'react';
 import Button from '../Button/Button';
 
-function JournalForm() {
-	const [inputData, setInputData] = useState('');
-
-	const inputText = (e) => {
-		console.log(e.target.value);
-		setInputData(e.target.value)
-	}
-
+function JournalForm({onSubmit}) {
 	const addJournalItem = (e) => {
 		e.preventDefault();
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData)
+		onSubmit(formProps);
 		console.log(formProps);
 	}
 
@@ -21,7 +14,7 @@ function JournalForm() {
 		<form className='journal-form' onSubmit={addJournalItem}>
 			<input type='text' name='title'/>
 			<input type='date' name='date'/>
-			<input type='text' name='tag' value={inputData} onChange={inputText}/>
+			<input type='text' name='tag'/>
 			<textarea name='post' id="" cols='30' rows='10'></textarea>
 			<Button text='Сохранить'/>
 		</form>
