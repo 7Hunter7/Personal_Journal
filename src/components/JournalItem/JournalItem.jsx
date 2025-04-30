7328d373125f30f0544163c408c6529eb19e634c
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-30 07:09:03 +0300

    UPD(JournalItem): added using const

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,12 +1,16 @@
 import './JournalItem.scss';
 
 function JournalItem() {
+  const title = 'Подготовка к обновлению курсов';
+  const date = new Date();
+  const text = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, vitae dolores, ipsa, optio quibusdam unde laboriosam accusantium ratione dolore quasi delectus praesentium a quam quis quo eveniet architecto libero necessitatibus?'
+
   return (
     <div className='journal-item'>
-      <h2 className='journal-item__header'>Title</h2>
+      <h2 className='journal-item__header'>{title}</h2>
       <div className='journal-item__body'>
-        <div className='journal-item__date'>22.05.25</div>
-        <div className='journal-item__text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, vitae dolores, ipsa, optio quibusdam unde laboriosam accusantium ratione dolore quasi delectus praesentium a quam quis quo eveniet architecto libero necessitatibus?</div>
+        <div className='journal-item__date'>{date.toLocaleString()}</div>
+        <div className='journal-item__text'>{text}</div>
       </div>
     </div>
   );
