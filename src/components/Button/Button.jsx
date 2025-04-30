749110e8d81e70c2b71a320d743354db17eff3d1
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-30 17:37:08 +0300

    UPD(Button): using without 'useState()'

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,8 @@
 import './Button.scss';
-import {useState} from 'react';
 
-function Button() {
-	const [text, setText] = useState('Сохранить');
-
-	const save = () => {
-		setText('Закрыть')
-		console.log('Сохранение...');
-	}
-
+function Button({text}) {
 	return (
-		<button onClick={save} className='button accent'>{text}</button>
+		<button className='button accent'>{text}</button>
 	);
 }
 
