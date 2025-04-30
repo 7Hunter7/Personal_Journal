382e7f733376b3a8f070c7d8f62648656ca02380
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-30 08:57:48 +0300

    UPD(project): added new settings to 'eslint.config.js'

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,32 @@ import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
 
-  const data = [
-    {
-      title: 'Подготовка к обновлению курсов',
-      date: new Date(),
-      text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, vitae dolores, ipsa.'
-    },
-    {
-      title: 'Поход в горы',
-      date: new Date(),
-      text: 'Optio quibusdam unde laboriosam accusantium ratione dolore quasi delectus praesentium a quam quis quo eveniet architecto libero necessitatibus.'
-    },
-    {
-      title: 'Сщбеседование',
-      date: new Date(),
-      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio minima animi eos, inventore porro atque non adipisci fuga velit! Dolor harum eius sit, molestias repellendus rem qui sint voluptates possimus.'
-    }
-  ];
+	const data = [
+		{
+			title: 'Подготовка к обновлению курсов',
+			date: new Date(),
+			text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, vitae dolores, ipsa.'
+		},
+		{
+			title: 'Поход в горы',
+			date: new Date(),
+			text: 'Optio quibusdam unde laboriosam accusantium ratione dolore quasi delectus praesentium a quam quis quo eveniet architecto libero necessitatibus.'
+		},
+		{
+			title: 'Собеседование',
+			date: new Date(),
+			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio minima animi eos, inventore porro atque non adipisci fuga velit! Dolor harum eius sit, molestias repellendus rem qui sint voluptates possimus.'
+		}
+	];
 
-  return <>
-    <h1>Заголовок</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque at sequi possimus inventore doloribus vel quos aut officiis repellat quod? Ratione esse optio ut commodi quasi explicabo placeat laudantium animi.</p>
-    <Button/>
-    <JournalItem title = {data[0].title} date = {data[0].date} text = {data[0].text}/>
-    <JournalItem title = {data[1].title} date = {data[1].date} text = {data[1].text}/>
-    <JournalItem title = {data[2].title} date = {data[2].date} text = {data[2].text}/>
-  </>; 
+	return <>
+		<h1>Заголовок</h1>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque at sequi possimus inventore doloribus vel quos aut officiis repellat quod? Ratione esse optio ut commodi quasi explicabo placeat laudantium animi.</p>
+		<Button/>
+		<JournalItem title = {data[0].title} date = {data[0].date} text = {data[0].text}/>
+		<JournalItem title = {data[1].title} date = {data[1].date} text = {data[1].text}/>
+		<JournalItem title = {data[2].title} date = {data[2].date} text = {data[2].text}/>
+	</>; 
 }
 
 export default App;
