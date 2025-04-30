ca0daf0eab53e271d88b71f049cb6b7ac5f7d9e0
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-30 09:24:39 +0300

    UPD(Button): added onClick using save()

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,10 @@
 import './Button.scss';
 
 function Button() {
+	const save = () => console.log('Сохранение...')
+
 	return (
-		<button className='button accent'>Сохранить</button>
+		<button onClick={save} className='button accent'>Сохранить</button>
 	);
 }
 
