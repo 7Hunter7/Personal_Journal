16177b1cce6fc43aa5a1cd0dcc3c3dcf914d05a4
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-30 10:36:07 +0300

    UPD(App): added using new components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Button from './components/Button/Button';
 import CardButton from './components/CardButton/CardButton';
 import JournalItem from './components/JournalItem/JournalItem';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
+import Sidebar from './layouts/Sidebar/Sidebar';
+import Body from './layouts/Body/Body';
+import Header from './components/Header/Header';
 
 function App() {
 
@@ -26,20 +29,22 @@ function App() {
 	];
 
 	return <>
-		<h1>Заголовок</h1>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque at sequi possimus inventore doloribus vel quos aut officiis repellat quod? Ratione esse optio ut commodi quasi explicabo placeat laudantium animi.</p>
-		<Button/>
-		<JournalAddButton/>
-		<CardButton>
-			<JournalItem title = {data[0].title} date = {data[0].date} text = {data[0].text}/>
-		</CardButton>
-		<CardButton>
-			<JournalItem title = {data[1].title} date = {data[1].date} text = {data[1].text}/>
-		</CardButton>
-		<CardButton>
-			<JournalItem title = {data[2].title} date = {data[2].date} text = {data[2].text}/>
-		</CardButton>
-
+		<Sidebar>
+			<Header/>
+			<JournalAddButton/>
+			<CardButton>
+				<JournalItem title = {data[0].title} date = {data[0].date} text = {data[0].text}/>
+			</CardButton>
+			<CardButton>
+				<JournalItem title = {data[1].title} date = {data[1].date} text = {data[1].text}/>
+			</CardButton>
+			<CardButton>
+				<JournalItem title = {data[2].title} date = {data[2].date} text = {data[2].text}/>
+			</CardButton>
+		</Sidebar>
+		<Body>
+			<Button/>
+		</Body>
 	</>; 
 }
 
