15d519eec4b1a1b903ffb89bd84beea05b74c430
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-01 09:09:01 +0300

    UPD(JournalForm): updated form validate

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -16,13 +16,13 @@ function JournalForm({onSubmit}) {
 		const formProps = Object.fromEntries(formData)
 		// Валидация данных
 		let isValidForm = true;
-		if (!formProps.title?.trim().lenght) {
+		if (!formProps.title?.trim().length) {
 			setFormValidState(state => ({...state, title: false}));
 			isValidForm = false;
 		} else {
 			setFormValidState(state => ({...state, title: true}));
 		}
-		if (!formProps.text?.trim().lenght) {
+		if (!formProps.text?.trim().length) {
 			setFormValidState(state => ({...state, text: false}));
 			isValidForm = false;
 		} else {
@@ -41,10 +41,10 @@ function JournalForm({onSubmit}) {
 
 	return (
 		<form className='journal-form' onSubmit={addJournalItem}>
-			<input type='text' name='title' style={{border: formValidState.title ? 'undefined' : '1px solid red'}}/>
-			<input type='date' name='date'style={{border: formValidState.date ? 'undefined' : '1px solid red'}}/>
+			<input type='text' name='title' style={{border: formValidState.title ? undefined : '1px solid red'}}/>
+			<input type='date' name='date'style={{border: formValidState.date ? undefined : '1px solid red'}}/>
 			<input type='text' name='tag'/>
-			<textarea name='text' id="" cols='30' rows='10' style={{border: formValidState.text ? 'undefined' : '1px solid red'}}/>
+			<textarea name='text' id="" cols='30' rows='10' style={{border: formValidState.text ? undefined : '1px solid red'}}/>
 			<Button text='Сохранить'/>
 		</form>
 
