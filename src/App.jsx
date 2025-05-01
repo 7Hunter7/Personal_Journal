b0cf3e3f8a326561e1154461b5f95d86ebebfa83
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-01 08:17:23 +0300

    UPD(App): updated id in addItem()

--- src/App.jsx
+++ src/App.jsx
@@ -34,11 +34,10 @@ function App() {
 
 	const addItem = item => {
 		setItems(oldItems => [...oldItems, {
-			id: Math.max(...oldItems.map(i => i.id)) + 1,
+			id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1,
 			title: item.title,
 			date: new Date(item.date),
 			text: item.text,
-
 		}])
 	}
 
