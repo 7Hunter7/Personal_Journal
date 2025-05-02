895e484383b6c55a12fe3d224f1b822aa2d984c1
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-02 13:04:38 +0300

    UPD(Input): added using ...props

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,12 +2,12 @@ import styles from './Input.module.scss';
 import cn from 'classnames';
 
 
-function Input({ className, isValid, appearence }) {
+function Input({ className, isValid, appearence, ...props }) {
 	return (
 		<input className={cn(className, styles['input'], {
 			[styles['input-title']]: appearence === 'title',
 			[styles['invalid']]: !isValid,
-		})}/>
+		})} {...props}/> // Передаем остальные свойства
 	);
 }
 
