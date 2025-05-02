17315fc33ea2a743a35c30e6ae82d6f6b990e965
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-02 13:19:43 +0300

    UPD(Input): added isValid=true for props

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,7 +2,7 @@ import { forwardRef } from 'react';
 import styles from './Input.module.scss';
 import cn from 'classnames';
 
-const Input = forwardRef(function Input({ className, isValid, appearence, ...props }, ref) {
+const Input = forwardRef(function Input({ className, isValid = true, appearence, ...props }, ref) {
 	return (
 		<input ref={ref} className={cn(className, styles['input'], {
 			[styles['input-title']]: appearence === 'title',
