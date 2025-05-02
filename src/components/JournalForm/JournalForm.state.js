e4678434c8ce3163d0873569bc2ab297bbeed8c1
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-02 08:44:38 +0300

    UPD(JournalForm.state): added case 'SUBMIT'

--- src/components/JournalForm/JournalForm.state.js
+++ src/components/JournalForm/JournalForm.state.js
@@ -16,5 +16,20 @@ export function formReducer(state, action) {
 	switch(action.type) {
 	case 'RESET_VALIDITY': 
 		return { ...state, isValid: INITIAL_STATE.isValid}
+	case 'SUBMIT': {
+		const titleValidity = action.payload.title?.trim().length;
+		const postValidity = action.payload.post?.trim().length;
+		const dateValidity = action.payload.date;
+    
+		return {
+			values:action.payload,
+			isValid: {
+				title: titleValidity,
+				post: postValidity,
+				date: dateValidity,
+			},
+			isFormReadyToSubmit: titleValidity && postValidity && dateValidity,
+		};
+	}
 	}
 }
