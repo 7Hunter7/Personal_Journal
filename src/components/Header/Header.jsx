7250827bf686595bfeb6d06cc40c4be199723479
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-03 09:23:25 +0300

    UPD: added SelectMonth component and using it in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,14 @@
+import SelectMonth from '../SelectMonth/SelectMonth';
 import './Header.scss';
 
-function Header({changeвМonth}) {
-	const changeМonth = (e) => {
-		changeвМonth(e.target.value)
-	}
+function Header({changedМonth}) {
 
 	return (
 		<>
 			<h1 className='header'>
 				Personal Journal
 			</h1>
-			<select name='user' id='user' onChange={changeМonth}>
-				<option value="1">Январь</option>
-				<option value="2">Февраль</option>
-				<option value="3">Март</option>
-				<option value="4">Апрель</option>
-				<option value="5">Май</option>
-				<option value="6">Июнь</option>
-				<option value="7">Июль</option>
-				<option value="8">Август</option>
-				<option value="9">Сентябрь</option>
-				<option value="10">Октябрь</option>
-				<option value="11">Ноябрь</option>
-				<option value="12">Декабрь</option>
-			</select>
+			<SelectMonth changedМonth={changedМonth}/>
 		</>
 	);
 }
---
+++ src/components/SelectMonth/SelectMonth.jsx
@@ -0,0 +1,26 @@
+import './SelectMonth.scss';
+
+function SelectMonth({changedМonth}) {
+	const changeМonth = (e) => {
+		changedМonth(e.target.value)
+	}
+
+	return (
+		<select name='user' id='user' onChange={changeМonth}>
+			<option value="1">Январь</option>
+			<option value="2">Февраль</option>
+			<option value="3">Март</option>
+			<option value="4">Апрель</option>
+			<option value="5">Май</option>
+			<option value="6">Июнь</option>
+			<option value="7">Июль</option>
+			<option value="8">Август</option>
+			<option value="9">Сентябрь</option>
+			<option value="10">Октябрь</option>
+			<option value="11">Ноябрь</option>
+			<option value="12">Декабрь</option>
+		</select>
+	);
+}
+
+export default SelectMonth;
