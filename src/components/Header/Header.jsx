5e4972be5f0e3c8bb378c005481bec4d3e391cb6
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-03 20:40:39 +0300

    UPD(Header): updated style file to module, added using useState() for set logos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import SelectMonth from '../SelectMonth/SelectMonth';
-import './Header.scss';
+import styles from './Header.module.scss';
+
+const logos = ['./logo.svg', './vite.svg'];
 
 function Header() {
+	const [logoIndex, setLogoIndex] = useState(0);
 
 	return (
 		<>
+			<img className={styles.logo} src={logos[logoIndex]} alt="Логотип журнала" />
 			<h1 className='header'>
 				Personal Journal
 			</h1>
