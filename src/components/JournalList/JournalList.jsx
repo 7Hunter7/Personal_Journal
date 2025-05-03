2ff5ab496367901b628759a8c67da0319542b56e
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-03 21:41:52 +0300

    UPD(JournalList): added filteredItems() and useMemo() using for memo it

--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,15 +1,11 @@
 import './JournalList.scss';
 import CardButton from '../CardButton/CardButton';
 import JournalItem from '../JournalItem/JournalItem';
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import { MonthContext } from '../../context/month.context';
 
 function JournalList({items}) {
 	const { monthId } = useContext(MonthContext);
-
-	if (items.length === 0) {
-		return <p>Записей пока нет, добавьте первую</p>;
-	}
 
 	// Функция сортировки
 	const sortItems = (a, b) => {
@@ -20,19 +16,25 @@ function JournalList({items}) {
 		}
 	};
 
+	// Фильтрация и сортировка записей
+	const filteredItems = useMemo(() => items
+		.filter(el => el.monthId === monthId)
+		.sort(sortItems), [items, monthId]);
+
+	if (items.length === 0) {
+		return <p>Записей пока нет, добавьте первую</p>;
+	}
+
 	return <>
-		{items
-			.filter(el => el.monthId === monthId)
-			.sort(sortItems)
-			.map(el => (
-				<CardButton key={el.id}>
-					<JournalItem 
-						title={el.title}
-						post={el.post}
-						date={el.date}
-					/>
-				</CardButton>
-			))}
+		{filteredItems.map(el => (
+			<CardButton key={el.id}>
+				<JournalItem 
+					title={el.title}
+					post={el.post}
+					date={el.date}
+				/>
+			</CardButton>
+		))}
 	</>
 }
 
