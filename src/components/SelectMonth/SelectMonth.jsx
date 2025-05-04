fc11d860dfab033b93c45217b4e58b07fe75b2ba
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-04 17:55:59 +0300

    UPD(SelectMonth): added styles for select

--- src/components/SelectMonth/SelectMonth.jsx
+++ src/components/SelectMonth/SelectMonth.jsx
@@ -1,6 +1,6 @@
-import './SelectMonth.scss';
 import { useContext } from 'react';
 import { MonthContext } from '../../context/month.context';
+import styles from'./SelectMonth.module.scss';
 
 function SelectMonth() {
 	const { monthId, setMonthId } = useContext(MonthContext);
@@ -10,7 +10,7 @@ function SelectMonth() {
 	}
 
 	return (
-		<select name='month' id='month' value={monthId} onChange={changeМonth}>
+		<select name='month' id='month' value={monthId} onChange={changeМonth} className={styles['select']}>
 			<option value="1">Январь</option>
 			<option value="2">Февраль</option>
 			<option value="3">Март</option>
