f33b14bb22073eda247d0d1cbb920f3021baa647
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-04 17:05:53 +0300

    UPD(JournalForm): updated delete button using 'data.id &&'

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -82,14 +82,14 @@ function JournalForm({onSubmit, data}) {
 		<form className={styles['journal-form']} onSubmit={addJournalItem}>
 			<div className={styles['form-row']}>
 				<Input type='text' ref={titleRef} isValid={isValid.title} value={values.title} onChange={onChange} name='title' appearence='title'/>
-				<button className={styles['form-delete']}>
+				{data.id && <button className={styles['form-delete']} type='button'> 
 					<svg 
 						xmlns="http://www.w3.org/2000/svg" 
 						viewBox="0 0 24 24"
 						fill="#fff">
 						<path d="m9.4 16.5l2.6-2.6l2.6 2.6l1.4-1.4l-2.6-2.6L16 9.9l-1.4-1.4l-2.6 2.6l-2.6-2.6L8 9.9l2.6 2.6L8 15.1zM7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21z"/>
 					</svg>
-				</button>
+				</button>}
 			</div>
 			<div className={styles['form-row']}>
 				<label htmlFor="date" className={styles['form-label']} >
