dcc7ace21e57c578449574a3f741ac2e5473437d
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-04 18:52:16 +0300

    FIX(JournalItem): updated styles to module

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,14 +1,14 @@
-import './JournalItem.scss';
+import styles from './JournalItem.module.scss'
 
 function JournalItem({title, post, date}) {
 	const formatedDate = new Intl.DateTimeFormat('ru-RU').format(date);
 
 	return (
 		<>
-			<h2 className='journal-item__header'>{title}</h2>
-			<div className='journal-item__body'>
-				<div className='journal-item__date'>{formatedDate}</div>
-				<div className='journal-item__text'>{post}</div>
+			<h2 className={styles['journal-item__header']}>{title}</h2>
+			<div className={styles['journal-item__body']}>
+				<div className={styles['journal-item__date']}>{formatedDate}</div>
+				<div className={styles['journal-item__text']}>{post}</div>
 			</div>
 		</>
 	);
